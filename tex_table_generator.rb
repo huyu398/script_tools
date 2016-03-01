c23198f104d67407823a133ee3eb63254cb6aad3
huyu398 <huyu.sakuya4645@gmail.com>
2016-03-01 22:02:40 +0900

    Specify input file path by command-line argument

--- tex_table_generator.rb
+++ tex_table_generator.rb
@@ -7,9 +7,21 @@ OptionParser.new do |opt|
   opt.parse!(ARGV)
 end
 
-CSV_FILE = Pathname.pwd + 'test.csv'
+exit 1 unless ARGV.size == 1
+
+CSV_FILE = Pathname.new(ARGV[0]).expand_path
 TEMPLATE_FILE = Pathname.pwd + 'table_template.tex.erb'
 OUTPUT_FILE = Pathname.pwd + 'table_output.tex'
+
+# input file check
+unless CSV_FILE.exist?
+  STDERR.puts "missing #{CSV_FILE}"
+  exit 1
+end
+unless CSV_FILE.file?
+  STDERR.puts "#{CSV_FILE} is a directory"
+  exit 1
+end
 
 table = CSV.read(CSV_FILE)
 columns = table.max_by { |e| e.size } .size
