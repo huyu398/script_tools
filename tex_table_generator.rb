13ee2b7dc6d2156a666bd37f85867cc7d073f9c1
huyu398 <huyu.sakuya4645@gmail.com>
2016-03-27 05:51:22 +0900

    Implement of setting for frame border

--- tex_table_generator.rb
+++ tex_table_generator.rb
@@ -11,6 +11,33 @@ OptionParser.new do |opt|
   opt.on('-d', '--docmute', 'Use docmute package in output file.') do |v|
     option[:docmute] = v
   end
+  opt.on('-f', '--frame=PATTERN', /(l?)([tT]?)([bB]?)(r?)/,
+         "Set the pattern of table's frame border (it's not grid lines).",
+         "You can set left, top, bottom and right frame, and bold frame only top and bottom",
+         "ex) --frame=ltbr : with left, top, bottom, and right frame",
+         "    --frame=TB   : with top and bottom bold frame",
+         "    --frame=rT   : this setting order is invalid",
+         "    --frame=Tr   : this setting order is valid"
+        ) do |v|
+    # mapping table from character to word
+    mapping = {
+      l: :left,
+      t: :top,
+      T: :top_bold,
+      b: :bottom,
+      B: :bottom_bold,
+      r: :right
+    }
+    # remove unnecessary elements
+    v.shift
+    v.delete('')
+
+    frame = {}
+    v.each do |p|
+      frame[mapping[p.to_sym]] = true
+    end
+    option[:frame] = frame
+  end
   opt.parse!(ARGV)
 end
 
@@ -38,11 +65,11 @@ table.each do |row|
 end
 
 caption = option[:caption] || File.basename(CSV_FILE, File.extname(CSV_FILE))
-dimensions = []
-(0..columns).each do
-  dimensions << 'c'
+dimensions = 'c|' * (columns - 1) + 'c'
+if option[:frame]
+  dimensions =  '|' + dimensions if option[:frame][:left]
+  dimensions =  dimensions + '|' if option[:frame][:right]
 end
-dimensions = '|' + dimensions * '|' + '|'
 
 template= File.read(TEMPLATE_FILE)
 File.write(OUTPUT_FILE, ERB.new(template, nil, '-').result(binding))
